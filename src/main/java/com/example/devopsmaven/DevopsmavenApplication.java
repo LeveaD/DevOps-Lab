913d72e6a957f35fce7e1bacdf17399852a14a83
LourdhSanya <lourdhsanya.27csb@licet.ac.in>
2026-02-05 11:22:06 +0530

    Initial commit - project setup with Spring Boot

---
+++ src/main/java/com/example/devopsmaven/DevopsmavenApplication.java
@@ -0,0 +1,13 @@
+package com.example.devopsmaven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopsmavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevopsmavenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/devopsmaven/DevopsmavenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.devopsmaven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevopsmavenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
